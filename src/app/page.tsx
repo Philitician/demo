f1c5bd3b48571b778228fdcf5726dba90076fd5f
Philip Rothschild <philiprene89@gmail.com>
2023-07-23 17:05:13 +0200

    change to vercel_env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {process.env.NODE_ENV}
+      {process.env.VERCEL_ENV}
     </main>
   );
 }
